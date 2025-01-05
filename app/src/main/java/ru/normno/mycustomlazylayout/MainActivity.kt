5dae5cffc08f055c7c7e5c5e50fa4039db9ff89b
NORMss <serega.b301@gmail.com>
2025-01-06 01:28:10 +0700

    feat: created custom lazy layout
    Closes #1

--- app/src/main/java/ru/normno/mycustomlazylayout/MainActivity.kt
+++ app/src/main/java/ru/normno/mycustomlazylayout/MainActivity.kt
@@ -4,44 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.normno.mycustomlazylayout.ui.theme.MyCustomLazyLayoutTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyCustomLazyLayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                ) { padding ->
+                    val state = viewModel.state.collectAsState()
+                    CustomLazyLayoutScreen(
+                        state = state.value,
+                        actions = viewModel,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
                     )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCustomLazyLayoutTheme {
-        Greeting("Android")
     }
 }
